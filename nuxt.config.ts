d0b09f97951689261bbb6e59e4da6b11c741a826
Sharmae09 <sharmae.code@gmail.com>
2025-06-30 09:42:22 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
+  css: ['vuetify/styles'],
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
 })
